b97b6ed6e39be1a7f8795dedc47686ef8111029f
iremuznbz <iremuzunbaz@gmail.com>
2014-07-10 12:48:15 +0300

    add hour enum

---
+++ src/cinema/model/Hour.java
@@ -0,0 +1,18 @@
+package cinema.model;
+
+public enum Hour {
+	TEN(10),TWELVE(12),FOURTEEN(14),SIXTEEN(16),EIGHTEEN(18),TWENTY(20),TWENTYTWO(22);
+	
+	public int hour;
+
+	private Hour(int hour) {
+		this.hour = hour;
+	}
+
+	public int getHour() {
+		return hour;
+	}
+	
+	
+	
+}
